51bf2f8bfedd4c7fbc38be81d2730150c421b3c1
Zizzard-The-Lizard <59262302+Zizzard-The-Lizard@users.noreply.github.com>
2022-03-28 19:34:22 -0400

    Step 3
    Step 3

--- budget/Expense.py
+++ budget/Expense.py
@@ -46,6 +46,8 @@ class Expenses():
         necessary_expenses = {x for x in self.list 
                                     if x.category == 'Phone' or x.category == 'Auto and Gas' or 
                                         x.category == 'Classes'  or x.category == 'Utilities' or 
-                                        x.category == 'Mortgage'}     
+                                        x.category == 'Mortgage'}
+        food_expenses = {x for x in self.list
+                            if x.category == 'Grocieries' or x.category == 'Eating Out'} 
 
     
