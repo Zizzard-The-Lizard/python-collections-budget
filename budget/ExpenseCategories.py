d4c401e4e57bb40a6b23a0a469824fe4ee442992
Zizzard-The-Lizard <59262302+Zizzard-The-Lizard@users.noreply.github.com>
2022-03-28 21:07:59 -0400

    Update
    Update

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,3 +1,4 @@
+from ast import Pass
 from . import Expense
 import matplotlib.pyplot as plt
 
@@ -9,6 +10,11 @@ def main():
 
     if divided_set_comp != divided_for_loop:
         print("Sets are NOT equal by == test")
+    
+    for a, b in zip(divided_for_loop, divided_set_comp):
+        if not (a.issubset(b) and 
+            b.issubset(a)):
+            print("Sets are NOT equal by subset test")
 
 
 if __name__ == "__main__":
